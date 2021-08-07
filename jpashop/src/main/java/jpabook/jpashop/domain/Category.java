14768aa56eeece356149152a4915d106194595ea
= <wjdrlans4000@naver.com>
2021-08-07 23:27:16 +0900

    refactor : 연관관계 매핑 예제 작성
    order <-> delivery 1:1 연관관계 매핑
    category <-> item 다:다 연관관계 매핑

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    //상위 카테고리
+    @ManyToOne
+    @JoinColumn(name = "PARENT_ID")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+
+    //실전에서는 다대다 사용 지양
+    @ManyToMany
+    @JoinTable(name = "CATEGORY_ITEM",
+                joinColumns = @JoinColumn(name = "CATEGORY_ID"),
+                inverseJoinColumns = @JoinColumn(name = "ITEM_ID")
+    )
+    private List<Item> items = new ArrayList<>();
+
+}
