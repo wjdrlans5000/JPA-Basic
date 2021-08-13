407e74d0bb54f109dd8681d11eafcb3bf9dc6867
= <wjdrlans4000@naver.com>
2021-08-13 23:21:32 +0900

    feat : 상속관계 매핑
    조인전략, 단일테이블 전략

--- README.md
+++ README.md
@@ -677,8 +677,51 @@ CREATE TABLE MEMBER (
         - 슈퍼타입 서브타입 관계라는 모델링 기법이 객체 상속과 유사
         - 상속관계 매핑: 객체의 상속과 구조와 DB의 슈퍼타입 서브타입 관계를 매핑
         - 슈퍼타입 서브타입 논리 모델을 실제 물리 모델로 구현하는 방법
-            - 각각 테이블로 변환 -> 조인 전략
-            - 통합 테이블로 변환 -> 단일 테이블 전략
-            - 서브타입 테이블로 변환 -> 구현 클래스마다 테이블 전략
+            - 각각 테이블로 변환 -> 조인 전략 (@Inheritance(strategy = InheritanceType.JOINED)
+            - 통합 테이블로 변환 -> 단일 테이블 전략(jpa 기본)
+                - @DiscriminatorColumn 없어도 DTYPE이 필수로 생성됨 
+            - 서브타입 테이블로 변환 -> 구현 클래스마다 테이블 전략 (@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+                - 부모클래스는 추상클래스로 선언
+                - @DiscriminatorColumn 의미없음 
+                ```java
+                  @Entity
+                  @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+                  public abstract class Item {
+                      ...
+                  }
+                ```
 
-    
+        - 주요 애노테이션
+            - @Inheritance(strategy=InheritanceType.XXX) 
+                - JOINED: 조인 전략
+                - SINGLE_TABLE: 단일 테이블 전략
+                - TABLE_PER_CLASS: 구현 클래스마다 테이블 전략
+            - @DiscriminatorColumn(name="DTYPE")
+            - @DiscriminatorValue("XXX")
+            
+        - 장,단점
+            - 조인전략(추천)
+                - 장점
+                    - 테이블이 정규화되어 있음
+                    - 외래키 참조 무결성 제약조건 활용가능
+                    - 저장공간 효율화
+                - 단점
+                    - 조회시 조인을 많이 사용, 성능저하
+                    - 조회 쿼리가 복잡해짐
+                    - 데이터 저장시 insert sql 2번 호출 (그렇게 큰 단점은 아니라고 생각함)
+            - 단일 테이블 전략
+                - 장점
+                    - 조인이 필요 없으므로 일반적으로 조회 성능이 빠름
+                    - 조회 쿼리가 단순함
+                - 단점
+                    - 자식 엔티티가 매핑한 컬럼은 모두 null 허용(치명적)
+                    - 단일 테이블에 모든 것을 저장하므로 테이블이 커질 수 있다. 
+                    상황에 따라서 조회 성능이 오히려 느려질 수 있다.
+            - 구현 클래스마다 테이블 전략(사용지양)
+                - 장점
+                    - 서브 타입을 명확하게 구분해서 처리할 때 효과적
+                    - not null 제약조건 사용 가능
+                - 단점
+                    - 여러 자식 테이블을 함께 조회할 때 성능이 느림(UNION SQL 필요)
+                    - 자식 테이블을 통합해서 쿼리하기 어려움
+
--- ex1-hello-jpa/src/main/java/hellojpa/jpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/jpaMain.java
@@ -49,21 +49,21 @@ public class jpaMain {
             // commit 시점에 데이터베이스에 쿼리가 날라감
 
             //연관관계 매핑 기초
-            Team team = new Team();
-            team.setName("TeamA");
+//            Team team = new Team();
+//            team.setName("TeamA");
 
-            em.persist(team);
+//            em.persist(team);
 
-            Member member = new Member();
-            member.setUsername("member1");
+//            Member member = new Member();
+//            member.setUsername("member1");
             //연관관계 편의 메서드가 양쪽다 있으면 문제가 될수 있기때문에 하나는 지움
 //            member.changeTeam(team); //**
             //객체지향스럽지 못한 방식
 //            member.setTeamId(team.getId());
-            em.persist(member);
+//            em.persist(member);
 
             //연관관계 편의 메서드
-            team.addMember(member);
+//            team.addMember(member);
 
             //연관관계의 주인이 아니기때문에 조회만 가능
             //객체지향적으로 생각했을때 양쪽 모두 값을 셋팅해주는 것이 맞음
@@ -77,14 +77,14 @@ public class jpaMain {
 
             //flush, clear를 하지 않으면 team에 member가 없음
             //영속성 컨텍스트에 em.persist(team); 시점의 객체가 1차 캐시로 로딩되어있기 때문에 team에 member 컬렉션이 비어있음
-            Team findTeam = em.find(Team.class, team.getId()); // 1차 캐시
-            List<Member> members = findTeam.getMembers();
+//            Team findTeam = em.find(Team.class, team.getId()); // 1차 캐시
+//            List<Member> members = findTeam.getMembers();
 
 //            Member findMember = em.find(Member.class, member.getId());
 //            List<Member> members = findMember.getTeam().getMembers();
-            for (Member m : members){
-                System.out.println("m = " + m.getUsername());
-            }
+//            for (Member m : members){
+//                System.out.println("m = " + m.getUsername());
+//            }
 
             //멤버에서 바로 팀을 꺼내서 사용할수 있음
 //            Team findTeam = findMember.getTeam();
@@ -94,6 +94,22 @@ public class jpaMain {
             //객체지향스럽지 못한 방식
 //            Long findTeamId = findMember.getTeamId();
 //            Team findTeam = em.find(Team.class,findTeamId);
+
+            // 상속관계 매핑 > 조인전략
+            Movie movie = new Movie();
+            movie.setDirector("aaaa");
+            movie.setActor("bbbb");
+            movie.setName("바람과함께사라지다");
+            movie.setPrice(10000);
+
+            em.persist(movie);
+
+            //1차캐시 제거
+            em.flush();
+            em.clear();
+
+            Movie findMove = em.find(Movie.class, movie.getId());
+            System.out.println("findMove = " + findMove);
 
 
             tx.commit();
